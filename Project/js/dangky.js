84802547b9d7ab7d2d397bbf4920c500ab27e26e
lionheartmettle <43745212+lionheartmettle@users.noreply.github.com>
2018-10-15 15:42:34 +0700

    lam xong pop-up dang ky

---
+++ Project/js/dangky.js
@@ -0,0 +1,52 @@
+function dangkynhanh()                                    
+{ 
+    var name = document.forms["RegForm"]["Name"];               
+    var phone = document.forms["RegForm"]["Telephone"];
+	var email = document.forms["RegForm"]["EMail"];    
+      
+    var what =  document.forms["RegForm"]["Subject"];  
+       
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+   
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+       
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    	
+	if (!validateEmail(email.value ))                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+     
+       
+    if (what.selectedIndex < 1)                  
+    { 
+        alert("Please enter your course."); 
+        what.focus(); 
+        return false; 
+    } 
+   
+    return true; 
+}
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
